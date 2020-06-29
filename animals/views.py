b5c063b76a20af096f49dfb1bda267c37d6de6d8
AlIld <gubaewa.alsu@yandex.ru>
2020-06-29 22:53:22 +0300

    Commit

--- animals/views.py
+++ animals/views.py
@@ -1,24 +1,30 @@
+import requests
 from django.shortcuts import render
+
 
 def home_screen_view(request):
     context = {}
 
     return render(request, 'home_page/page.html', context)
 
+
 def animals_view(request):
     context = {}
 
     return render(request, 'animals/animals.html', context)
+
 
 def cat_view(request):
     context = {}
 
     return render(request, '#', context)
 
+
 def dog_view(request):
     context = {}
 
     return render(request, '#', context)
+
 
 def fox_view(request):
     context = {}
